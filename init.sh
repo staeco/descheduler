606767ff614d838c3863a8fbb8a4296585718c4d
Steve Lacy <me@slacy.me>
2019-03-08 10:17:27 -0700

    Add job spec

---
+++ init.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cat << EOF| kubectl create -f -
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1beta1
+metadata:
+  name: descheduler-cluster-role
+rules:
+- apiGroups: [""]
+  resources: ["nodes"]
+  verbs: ["get", "watch", "list"]
+- apiGroups: [""]
+  resources: ["pods"]
+  verbs: ["get", "watch", "list", "delete"]
+- apiGroups: [""]
+  resources: ["pods/eviction"]
+  verbs: ["create"]
+EOF
+
+kubectl create sa descheduler-sa -n kube-system
+
+kubectl create clusterrolebinding descheduler-cluster-role-binding \
+    --clusterrole=descheduler-cluster-role \
+    --serviceaccount=kube-system:descheduler-sa
+
+kubectl create configmap descheduler-policy-configmap \
+     -n kube-system --from-file=./examples/policy.yaml
+
+kubectl create -f job.yaml
